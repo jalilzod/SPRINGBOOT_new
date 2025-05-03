b2326745d877f150c6fe7f26a3d2686e357f45c6
jalilzod_art <mingyuan_12@qq.com>
2025-05-03 23:30:18 +0800

    added REST API

---
+++ 04-store-real-app/demo/src/main/java/com/jalilzod_art/store/repositories/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.jalilzod_art.store.services;
+
+public interface CategoryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.jalilzod_art.store.entities.Category, java.lang.Byte> {
+  }
---
+++ 04-store-real-app/demo/src/main/java/com/jalilzod_art/store/repositories/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.jalilzod_art.store.services;
+
+public interface ProductRepository extends org.springframework.data.jpa.repository.JpaRepository<com.jalilzod_art.store.entities.Product, java.lang.Long> {
+  }
